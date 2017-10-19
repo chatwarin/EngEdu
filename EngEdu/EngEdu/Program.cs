f387c7cd682aab4ae8ccba5042ca59860db1019a
chatwarin <chatwarin97@gmail.com>
2017-10-19 19:48:15 +0700

    4th year semester 1

--- EngEdu/EngEdu/Program.cs
+++ EngEdu/EngEdu/Program.cs
@@ -16,6 +16,7 @@ namespace EngEdu
             Console.WriteLine("Year 2560 semester 2, I attend an orientation of the faculty.");
             Console.WriteLine("Year 2561 semester 1, I attend an internship orientation.");
             Console.WriteLine("Year 2561 semester 2, I got an internship with the company.");
+            Console.WriteLine("Year 2562 semester 1, I learn to professional teaching practice.");
         }
     }
 }
