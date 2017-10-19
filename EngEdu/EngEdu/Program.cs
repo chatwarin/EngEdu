546864b94ad89cac44a60871ce321e2f399dce23
chatwarin <chatwarin97@gmail.com>
2017-10-19 19:46:05 +0700

    3rd year semester 1

--- EngEdu/EngEdu/Program.cs
+++ EngEdu/EngEdu/Program.cs
@@ -14,7 +14,7 @@ namespace EngEdu
             Console.WriteLine("Year 255x semester 2, I learn to program in C sharp.");
             Console.WriteLine("Year 2560 semester 1, I have to regrade on some course.");
             Console.WriteLine("Year 2560 semester 2, I attend an orientation of the faculty.");
-
+            Console.WriteLine("Year 2561 semester 1, I attend an internship orientation.");
         }
     }
 }
